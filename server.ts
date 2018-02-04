c882b210184e312255aa6ed9e7907bbf0e2531d2
Eric Tuvesson <eric.tuvesson@gmail.com>
2018-02-04 01:34:18 +0100

    Add CommonJS

--- server.ts
+++ server.ts
@@ -8,14 +8,17 @@ const app = express()
 
 var port = process.env.port || 1337
 
-// Copy phaser to build/vendor
+// Copy libraries to build/vendor
 fs.createReadStream('node_modules/phaser-ce/build/phaser.min.js')
   .pipe(fs.createWriteStream('build/vendor/phaser.min.js'));
+fs.createReadStream('node_modules/common-js/common.min.js')
+  .pipe(fs.createWriteStream('build/vendor/common.min.js'));
 
+// Make all assets public
 app.use('/js',     express.static('build/js'))
 app.use('/assets', express.static('build/assets'))
 app.use('/vendor', express.static('build/vendor'))
-app.use('/src', express.static('src'))
+app.use('/src',    express.static('src'))
 
 fs.readFile('./build/index.html', (error, html) => {
     if (error) throw error;
@@ -25,6 +28,7 @@ fs.readFile('./build/index.html', (error, html) => {
         response.end();
     })
 
-    app.listen(port, () => console.log("Server running at => http://localhost:" + port + "/\nCTRL + C to shutdown"))
-
+    app.listen(port, () => {
+        console.log("Server running at => http://localhost:" + port + "/\nCTRL + C to shutdown")
+    })
 });
